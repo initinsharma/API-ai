39098862921e97062baff0b76b2aa29dda9e8322
initinsharma <nitin2412@gmail.com>
2017-12-03 18:12:27 +0530

    test

--- app.py
+++ app.py
@@ -81,7 +81,10 @@ def makeYqlQuery(req):
         return None
 
     #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-    return  "SELECT count(*) FROM `bigquery-public-data.irs_990.irs_990_ein`WHERE city ='" + city + "'"
+    #return  "SELECT count(*) FROM `bigquery-public-data.irs_990.irs_990_ein`WHERE city ='" + city + "'"
+    return 'SELECT count(*) FROM `bigquery-public-data.irs_990.irs_990_ein` '
+            'WHERE city = "SUNNYVALE" '
+             'LIMIT 2'
 
 
 def makeWebhookResult(data):
