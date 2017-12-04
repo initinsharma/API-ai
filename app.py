6267df18a95588be8ae42ad5abc176f9dcf145b5
initinsharma <nitin2412@gmail.com>
2017-12-04 11:52:12 +0530

    test

--- app.py
+++ app.py
@@ -101,7 +101,8 @@ def makeYqlQuery(req):
         return None
 
     #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-    return  "SELECT count(*) FROM `bigquery-public-data.irs_990.irs_990_ein`WHERE city ='" + city + "'"
+    return city
+    #return  "SELECT count(*) FROM `bigquery-public-data.irs_990.irs_990_ein` WHERE city = '" + city + "'"
     #return 'SELECT count(*) FROM `bigquery-public-data.irs_990.irs_990_ein`'
             #'WHERE city = "SUNNYVALE" '
             # 'LIMIT 2'
