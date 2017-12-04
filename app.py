1962d692dec9886d5dfa9ab8c3791041d72e38f5
initinsharma <nitin2412@gmail.com>
2017-12-04 16:56:23 +0530

    test

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def makeYqlQuery(req):
 
     #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
     #return city
-    return "SELECT count(*) FROM `bigquery-public-data.irs_990.irs_990_ein`"
+    return "SELECT count(*) FROM `bigquery-public-data.irs_990.irs_990_ein` WHERE city = 'SUNNYVALE'"
     #return  "SELECT count(*) FROM `bigquery-public-data.irs_990.irs_990_ein` WHERE city = '" + city + "'"
     #return 'SELECT count(*) FROM `bigquery-public-data.irs_990.irs_990_ein`'
             #'WHERE city = "SUNNYVALE" '
