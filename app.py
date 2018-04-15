49f9dc75ae239b6cd1ce934fecefff5558ca2378
initinsharma <nitin2412@gmail.com>
2018-04-15 11:32:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def processRequest(req):
             product = "Nivea"
         else:
             product = "Lakme"
-        speech = "Hi, Rigt Product for you is  " + undertone + " " + state + " " + color 
+        speech = "Hi, Rigt Product for you is  " + product 
         res =  { "speech": speech,
             "displayText": speech,
              "source": "apiai-weather-webhook-sample"
